dafbc893d1ee1620a3c0ee5357c82fe851ef267b
Yuki Tomioka <yuki_tomioka@zozoused.co.jp>
2019-09-20 17:03:48 +0900

    fix flow error
    型定義を追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 // @flow
 export class Hello {
-  constructor(name) {
+  name: string; // this.nameの型定義
+
+  constructor(name: string) { // 引数nameの型定義
     this.name = name;
     this.say();
   }
 
-  say() {
+  say(): void { // 戻り値の型定義、voidである場合は省略も可能
     console.log(`Hello ${this.name} World!`);
   }
 }
