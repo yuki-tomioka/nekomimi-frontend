e6a6fe09ec7bee6428c19d0d5b4328042f256cd2
Yuki Tomioka <yuki_tomioka@zozoused.co.jp>
2019-09-27 16:01:53 +0900

    say()メソッドのテスト

--- test/index.test.js
+++ test/index.test.js
@@ -10,5 +10,18 @@ describe('Hello Class Test', () => {
 
   test('We can check if the name defined the class constructor', () => {
     expect(hello.name).toBe(name);
-  })
+  });
+
+  // say()メソッドのテスト
+  test('We can check if console.log() called on say() method', () => {
+    const spy = jest.spyOn(console, 'log');
+
+    hello.say();
+
+    // spyしたメソッドが呼ばれていることを確認する
+    expect(spy).toHaveBeenCalledWith(`Hello ${name} World!`);
+
+    spy.mockReset();
+    spy.mockRestore();
+  });
 });
