1d6e86f89c22784acb82b2f80c3ffcdc589fd07c
Yuki Tomioka <yuki_tomioka@zozoused.co.jp>
2019-10-01 17:08:21 +0900

    webpackの設定を共通と開発用に分離

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+const src = path.join(__dirname, 'src');
+
+module.exports = {
+  entry: path.resolve(src, 'js/render.jsx'),
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]'
+              },
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(src, 'html/index.html')
+    }),
+    new MiniCSSExtractPlugin({
+      filename: 'app.css'
+    })
+  ]
+};
---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+const dist = path.join(__dirname, 'dist');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: 'index.bundle.js',
+    path: dist
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: dist,
+    hot: true,
+    port: 3000
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+});
